0e2a0b4156d6a52fbaaa25318c3b33cdd697c31f
c9lul12btw <mirass.steam@gmail.com>
2018-02-14 21:26:39 +0000

    yo

--- src/io/github/c9lul12btw/Main.java
+++ src/io/github/c9lul12btw/Main.java
@@ -1,8 +1,41 @@
 package io.github.c9lul12btw;
 
+import io.github.c9lul12btw.classes.euclid.*;
+
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner s = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        search();
+    }
+
+    private static void search() {
+
+        boolean correctSearch = false;
+        while (!correctSearch) {
+
+            System.out.println("SEARCH TYPES: ID, CLASS");
+            System.out.print("Enter search type: ");
+            String searchType = s.nextLine();
+
+            switch (searchType.toLowerCase()) {
+                case "id":
+                    correctSearch = true;
+                    System.out.println("\nSearching for SCP-ID.\n");
+                    break;
+                case "class":
+                    correctSearch = true;
+                    System.out.println("\nSearching for SCP-CLASS.\n");
+                    break;
+                default:
+                    correctSearch = false;
+                    System.out.println("\nPlease enter a correct value.\n");
+                    break;
+            }
+        }
     }
 }
---
+++ src/io/github/c9lul12btw/classes/euclid/scp096.java
@@ -0,0 +1,8 @@
+package io.github.c9lul12btw.classes.euclid;
+
+public class scp096 {
+
+    private String itemID = "SCP-096";
+    private String itemNick = "The Shy Guy";
+    private String objectClass = "Euclid";
+}
